ed34dbe60f73f541a245eb61e5145ead87184085
WebertySilveira <weberty.silveira.sousa@gmail.com>
2020-06-27 18:33:38 -0300

    CRUD: INSERT - OK

---
+++ Controller/insert.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "Connect.php";
+
+    try {
+
+        $nome = $_POST['nome'];
+        $preco = $_POST['preco'];
+        $descricao = $_POST['descricao'];
+        $imagem = $_POST['imagem'];
+
+
+        $sql = "INSERT INTO produtos (nome, preco, descricao, imagem)
+        VALUES ('$nome', '$preco', '$descricao', '$imagem')";
+
+        $conn->exec($sql);
+
+        header( "Refresh:0; ../index.php", true, 303);
+
+    } catch(PDOException $e) {
+
+    }
+?>
